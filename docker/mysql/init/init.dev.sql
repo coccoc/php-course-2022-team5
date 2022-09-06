57a4d4c21673f71b0f990966eca0b7c34fb3f255
duync <duync@coccoc.com>
2022-09-06 18:29:35 +0700

    Remove unneed initialize database configs

--- docker/mysql/init/init.dev.sql
+++
@@ -1,4 +0,0 @@
-CREATE DATABASE 'cinema-php';
-CREATE USER 'dev'@'localhost' IDENTIFIED BY 'dev==';
-GRANT ALL PRIVILEGES ON cinema-php.* TO 'dev'@'%' IDENTIFIED BY 'dev==';
-GRANT ALL PRIVILEGES ON cinema-php.* TO 'dev'@'localhost' IDENTIFIED BY 'dev==';
